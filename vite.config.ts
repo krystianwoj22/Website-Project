df8613fc1feef028e5f94c643b6ebfb6cb58e77c
krystianwoj22 <krystian7431@gmail.com>
2026-04-24 17:16:59 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    publicDir: 'static',
-    build: {
-      outDir: 'public',
-      emptyOutDir: true,
-    },
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
+    },
+    build: {
+      outDir: 'public',
+      emptyOutDir: true,
     },
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
